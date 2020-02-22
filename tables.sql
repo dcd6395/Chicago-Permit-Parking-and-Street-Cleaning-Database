153abbcd8fd8f76ec0e04805b68a88648324a676
stewart-lacy <56944681+stewart-lacy@users.noreply.github.com>
2020-02-22 12:24:34 -0600

    Add files via upload

--- tables.sql
+++ tables.sql
@@ -8,12 +8,15 @@ street_direction varchar(30),
 street_name varchar(30),
 street_type varchar(30),
 buffer varchar(30),
-ward_low int,
+ward int,
 ward_high int);
 
+Drop TABLE chicago_parking
 CREATE TABLE chicago_street_sweep(ward int,
 section int,
 month varchar(30),
 month_number int,
 dates varchar(30));
 
+select cp.zone, cp.odd_even,cp.address_range_low,cp.address_range_high,cp.street_direction,cp.street_name,cp.street_type, cp.buffer,cp.ward,cp.ward_high, css.section, css.month, css.month_number, css.dates from chicago_parking cp
+join chicago_street_sweep css on css.ward = cp.ward
